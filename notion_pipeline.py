56b12720e6990e973a829854546c21e190bb2a35
Joscha Feth <joscha@feth.com>
2025-03-20 09:08:42 +0000

    wip(today's fortune): You are always busy.

--- notion_pipeline.py
+++ notion_pipeline.py
@@ -9,8 +9,25 @@ def load_notion_data() -> None:
     pipeline = dlt.pipeline(
         pipeline_name="notion_pipeline", destination="duckdb", dev_mode=DEV_MODE
     )
+
+    def column_name_projection(
+        prop: DatabaseProperty, normalize: Callable[[str], str]
+    ) -> str:
+        result_name = normalize(prop.name)
+        if result_name in [
+            "my_column_name",
+        ]:
+            return None
+        return result_name
+
+    my_db = DatabaseResource(
+        database_id="12345678912345678912345678912345",
+        column_name_projection=column_name_projection,
+    )
+
     data = source(
         limit=-1 if not DEV_MODE else 1,
+        databases=[my_db],
     )
     info = pipeline.run(
         data,
